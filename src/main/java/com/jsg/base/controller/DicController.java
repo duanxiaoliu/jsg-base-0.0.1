ecd09890701db2cbad36d1cd87fb84ec01037e65
duanxiaoliu <1572787328@qq.com>
2016-05-26 17:18:52 +0800

    数据字曲新增

--- src/main/java/com/jsg/base/controller/DicController.java
+++ src/main/java/com/jsg/base/controller/DicController.java
@@ -1,5 +1,6 @@
 package com.jsg.base.controller;
 
+import javax.jws.WebParam.Mode;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -12,6 +13,7 @@ import com.jsg.base.model.BaseDic;
 import com.jsg.base.model.BasePage;
 import com.jsg.base.model.DicCategory;
 import com.jsg.base.service.IDicService;
+import com.jsg.base.util.DataUtil;
 import com.jsg.base.util.PageUtil;
 
 /**
@@ -53,6 +55,44 @@ public class DicController extends BaseController {
 		model.addAttribute("page", page);
 		model.addAttribute("dicCategory", dicCategory);
 		
-		return "dicManage/dicManage/queryDicInfo";
+		return "dicManage/dicManage/queryDicCategory";
 	}
+	/**
+	 * 
+	* @Title: editDicCategory 
+	* @Description: TODO(跳转到编辑页面) 
+	* @param @param request
+	* @param @param response
+	* @param @param model
+	* @param @return
+	* @return String
+	* @throws 
+	* @author duanws
+	* @date 2016-5-26 下午2:19:25
+	 */
+	@RequestMapping({"dicManage/dicManage/jsg-add/addDicCategory","dicManage/dicManage/jsg-update/editDicCategory"})
+	public String editDicCategory(HttpServletRequest request,HttpServletResponse response,ModelMap model){
+		String dicCateGoryId = request.getParameter("id");
+		DicCategory dicCategory = new DicCategory();
+		if(DataUtil.strIsNotNull(dicCateGoryId)){
+			dicCategory = this.dicSerivce.getDicCateGoryById(dicCateGoryId);
+		}	
+		this.setData(dicCategory, model);
+		return "dicManage/dicManage/editDicCategory";
+	}
+	/**
+	 * 
+	* @Title: setData 
+	* @Description: TODO(封闭页面显示元素) 
+	* @param @param dicCategory
+	* @param @param model
+	* @return void
+	* @throws 
+	* @author duanws
+	* @date 2016-5-26 下午2:28:34
+	 */
+	private void setData(DicCategory dicCategory,ModelMap model){
+		model.addAttribute("dicCategory", dicCategory);
+	}
+	
 }
